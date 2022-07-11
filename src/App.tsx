ab4a59284554784842deeff438eeec9aceeacd80
arts9303 <arts9303@gmail.com>
2022-07-11 14:56:46 +0300

    Accordion component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
 
 function App() {
     return (
@@ -32,35 +32,6 @@ function Rating() {
 
 function Star() {
     return <div>Star</div>;
-}
-
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    );
-}
-
-function AccordionTitle() {
-    return (
-        <>
-            <h3>Меню</h3>
-        </>
-    );
-}
-
-function AccordionBody() {
-    return (
-        <>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </>
-    );
 }
 
 export default App;
